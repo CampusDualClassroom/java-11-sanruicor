ead7ed1360aa98158ff7f892eec67e1035df935b
AdelaSRuizDeCortázar <sanruiade@gmail.com>
2024-09-27 12:45:17 +0200

    Ejercicio resuelto

--- src/main/java/com/campusdual/classroom/Exercise11.java
+++ src/main/java/com/campusdual/classroom/Exercise11.java
@@ -6,6 +6,28 @@ public class Exercise11 {
         TVRemote blackRemote = new TVRemote("Negro");
         System.out.println(redRemote.getColor());
         System.out.println(blackRemote.getColor());
+
+        //Encender la TV
+        redRemote.turnOn();
+        //Subir un canal
+        redRemote.channelUp();
+        //Bajar un canal
+        redRemote.channelDown();
+        //Establecer el canal en 0, intentar bajar un canal y comprobar que no se puede
+        redRemote.channel = 0;
+        redRemote.channelDown();
+        //Subir el volumen
+        redRemote.volumeUp();
+        //Bajar el volumen
+        redRemote.volumeDown();
+        //Establecer el volumen en 0, intentar bajar el volumen y comprobar que no se puede
+        redRemote.volume = 0;
+        redRemote.volumeDown();
+        //Obtener el color del mando a distancia y mostrarlo por pantalla
+        String color = redRemote.getColor();
+        System.out.println(color);
+        //Apagar la TV
+        redRemote.turnOff();
     }
 
 }
--- src/main/java/com/campusdual/classroom/TVRemote.java
+++ src/main/java/com/campusdual/classroom/TVRemote.java
@@ -1,5 +1,56 @@
 package com.campusdual.classroom;
 
 public class TVRemote {
+    int channel;
+    int volume;
+    boolean on;
+    String color;
 
+    public TVRemote(String color){
+        this.channel = 0;
+        this.volume = 20;
+        this.color = color;
+    }
+
+    public boolean turnOn(){
+        this.on = true;
+        return this.on;
+    }
+
+    public boolean turnOff(){
+        this.on = false;
+        return this.on;
+    }
+
+    public void channelUp(){
+        this.channel++;
+    }
+
+    public void channelDown(){
+        if(this.checkInValue(this.channel)){
+            this.channel--;
+        }
+    }
+
+    public void volumeUp(){
+        this.volume++;
+    }
+
+    public void volumeDown(){
+        if(this.checkInValue(this.volume)){
+            this.volume--;
+        }
+    }
+
+    public String getColor(){
+        return this.color;
+    }
+
+    private boolean checkInValue(int value){
+        if (value == 0){
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
